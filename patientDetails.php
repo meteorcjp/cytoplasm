ec90cb534260cb468ba6140a502ca44d3bbdabf4
meteorcjp <meteorcjp@gmail.com>
2014-01-20 05:54:17 +0000

    CHANGE DATA

--- patientDetails.php
+++ patientDetails.php
@@ -1,6 +1,6 @@
 <?php
 		$patientId = $_POST['userId'];
-		echo "ID: /".$patientId."/Name: /Mark James/Age: /40/Entry Date: /2014-01-01/Description: /Patient suffers from recurrent severe Major 
+		echo "ID: /".$patientId."/Name: /Irene Miller/Date of birth: /1958-06-23/Address: /30 Nell Lane, Manchester, Greater Manchester M21 7SJ, UK/Entry Date: /2014-01-01/Description: /Patient suffers from recurrent severe Major 
 Depressive Disorder/Available Sensor: /Heart Rate Sensor";
 			
 ?>
