5b09b4a233521eb18fe1078e008c1c1c7e682923
meteorcjp <meteorcjp@gmail.com>
2014-01-20 00:45:52 +0000

    test real username password http

--- js/script.js
+++ js/script.js
@@ -109,7 +109,7 @@ function update(data,totalPoints,updateInterval) {
 
 function submitForm() {
 
-   $.post("comp2013.hyperspacedesign.co.uk/api/login/index.php" ,
+   $.post("http://comp2013.hyperspacedesign.co.uk/api/login/index.php" ,
    {
     username : $('#username').val(),
     password : $('#password').val()
