91ca45420731042b144a2a13de425ba09f67e368
meteorcjp <meteorcjp@gmail.com>
2014-01-20 05:14:25 +0000

    docu.ready2

--- js/script.js
+++ js/script.js
@@ -64,48 +64,7 @@ function createPatientDetails(result) {
 
 }
 
-function getData(data,totalPoints) {
 
-      if (data.length > 0)
-        data = data.slice(1);
-
-      // Do a random walk
-
-      while (data.length < totalPoints) {
-
-        var prev = data.length > 0 ? data[data.length - 1] : 50,
-          y = prev + Math.random() * 10 - 5;
-
-        if (y < 0) {
-          y = 0;
-        } else if (y > 100) {
-          y = 100;
-        }
-
-        data.push(y);
-      }
-
-      // Zip the generated y values with the x values
-
-      var res = [];
-      for (var i = 0; i < data.length; ++i) {
-        res.push([i, data[i]])
-      }
-
-      return res;
-
-}
-
-function update(data,totalPoints,updateInterval) {
-
-      plot.setData([getData(data,totalPoints)]);
-
-      // Since the axes don't change, we don't need to call plot.setupGrid()
-
-      plot.draw();
-      setTimeout(update(data,totalPoints,updateInterval), updateInterval);
-      
-}
 
 function submitForm() {
 
@@ -190,4 +149,50 @@ $(document).ready(
 
   update(data,totalPoints,updateInterval);
 
-});
+}
+
+function getData(data,totalPoints) {
+
+      if (data.length > 0)
+        data = data.slice(1);
+
+      // Do a random walk
+
+      while (data.length < totalPoints) {
+
+        var prev = data.length > 0 ? data[data.length - 1] : 50,
+          y = prev + Math.random() * 10 - 5;
+
+        if (y < 0) {
+          y = 0;
+        } else if (y > 100) {
+          y = 100;
+        }
+
+        data.push(y);
+      }
+
+      // Zip the generated y values with the x values
+
+      var res = [];
+      for (var i = 0; i < data.length; ++i) {
+        res.push([i, data[i]])
+      }
+
+      return res;
+
+}
+
+
+function update(data,totalPoints,updateInterval) {
+
+      plot.setData([getData(data,totalPoints)]);
+
+      // Since the axes don't change, we don't need to call plot.setupGrid()
+
+      plot.draw();
+      setTimeout(update(data,totalPoints,updateInterval), updateInterval);
+      
+}
+
+);
