a910a5847759ff46f91eecb9fe1668dfcdd2b321
meteorcjp <meteorcjp@gmail.com>
2014-01-22 19:56:24 +0000

    add all

---
+++ patientDetails.php
@@ -0,0 +1,5 @@
+<?php
+		$patientId = $_POST['userId'];
+		echo "ID: /".$patientId."/Name: /Irene Miller/Date of birth: /1958-06-23/Address: /30 Nell Lane, Manchester, Greater Manchester M21 7SJ, UK/Entry Date: /2014-01-01/Description: /Heart Disease/Available Sensor: /HeartRateSensor";
+			
+?>
